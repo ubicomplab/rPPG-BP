03833d960d5d6f6086e6095cabc6f3da1c8bea4c
chengqian ma <cm74@uw.edu>
2023-02-20 21:22:12 -0800

    update readme

--- ppg_bp/train.sh
+++ ppg_bp/train.sh
@@ -1,32 +1,11 @@
 #!/bin/bash
 # 5 fold cross validation
-python train_fusion.py --config config_face_manual_demo.json
+python train_fusion.py --config ./configs/config_face_manual_demo.json
 
-python train_fusion.py --config config_face_manual_demo2.json
+python train_fusion.py --config ./configs/config_face_manual_demo2.json
 
-python train_fusion.py --config config_face_manual_demo3.json
+python train_fusion.py --config ./configs/config_face_manual_demo3.json
 
-python train_fusion.py --config config_face_manual_demo4.json
+python train_fusion.py --config ./configs/config_face_manual_demo4.json
 
-python train_fusion.py --config config_face_manual_demo5.json
-
-python test_fusion.py --config test_config_manual_demo.json
-# python train.py --config config_palm.json
-
-# python train.py --config config_palm2.json
-
-# python train.py --config config_palm3.json
-
-# python train.py --config config_palm4.json
-
-# python train.py --config config_palm5.json
-
-# python train.py --config config_face_palm.json
-
-# python train.py --config config_face_palm2.json
-
-# python train.py --config config_face_palm3.json
-
-# python train.py --config config_face_palm4.json
-
-# python train.py --config config_face_palm5.json
+python train_fusion.py --config ./configs/config_face_manual_demo5.json
